7dde56cad9895a79ffe5539fc45adc0f3cfa80d2
DANA LEILA MANDELBAUM <47574606@ort>
2023-08-22 09:02:37 -0300

    Creando

---
+++ EvaluacionMandelbaum/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EvaluacionMandelbaum.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
